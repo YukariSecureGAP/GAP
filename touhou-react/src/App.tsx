e757d85049108298dfd12e334b5e3a62828ddc05
憨批未来YY <52804925+Caroline520f@users.noreply.github.com>
2023-06-26 23:00:43 +0800

    fixed

--- touhou-react/src/App.tsx
+++ touhou-react/src/App.tsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <p>Hello Worlds</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
