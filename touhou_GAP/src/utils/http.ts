19008a60169dd10bfb4e8019cddab6f7ef66e402
憨批未来YY <52804925+Caroline520f@users.noreply.github.com>
2023-07-27 10:44:51 +0800

    添加了登录注册的接口

--- touhou_GAP/src/utils/http.ts
+++ touhou_GAP/src/utils/http.ts
@@ -3,7 +3,7 @@
 import axios from "axios";
 import { Constants } from "./constants";
 const API_BASE_URL = "http://121.37.17.108:5000/api";
-const REG_LOGIN_URL = "http://localhost:5001/api";
+const REG_LOGIN_URL = "http://121.37.17.108/";
 
 // 封装通用的GET请求方法
 const get = async <T>(url: string, params?: any): Promise<T> => {
@@ -27,12 +27,18 @@ export const getAllComics = async (): Promise<Constants.ComicInfo[]> => {
   const comics = await get<Constants.ComicInfo[]>(`${API_BASE_URL}/comics`);
   return comics;
 };
-
-export const  Register = async (username: string, password: string): Promise<any> => {
+export const Register = async (
+  username: string,
+  password: string,
+  authorization: string,
+  userAccount: string
+): Promise<any> => {
   try {
-    const response = await axios.post(REG_LOGIN_URL + "/register", {
+    const response = await axios.post(REG_LOGIN_URL + "register", {
       username,
       password,
+      authorization,
+      userAccount,
     });
     return response;
   } catch (error) {
@@ -41,11 +47,16 @@ export const  Register = async (username: string, password: string): Promise<any
   }
 };
 
-export const Login =  async (username: string, password: string): Promise<any> => {
+export const Login = async (
+  username: string,
+  password: string,
+  account: string
+): Promise<any> => {
   try {
-    const response = await axios.post(REG_LOGIN_URL + "/login", {
+    const response = await axios.post(REG_LOGIN_URL + "login", {
       username,
       password,
+      account,
     });
     return response.data;
   } catch (error) {
